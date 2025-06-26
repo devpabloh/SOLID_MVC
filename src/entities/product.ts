51aeea13d74344d57aceb6761fe2ea34d72c3519
pablo <devpabloh@gmail.com>
2025-06-26 00:07:09 -0300

    feat: estrutura inicial do projeto

---
+++ src/entities/product.ts
@@ -0,0 +1,35 @@
+export type ProductProps = {
+    id: string;
+    name: string;
+    price: number;
+    quantity: number;
+}
+
+export class Product{
+    private constructor (readonly props: ProductProps){}
+
+    public static create(name: string, price:number){
+        return new Product({
+            id: crypto.randomUUID().toString(),
+            name,
+            price,
+            quantity: 0,
+        })
+    }
+
+    public get id(){
+        return this.props.id
+    }
+
+    public get name(){
+        return this.props.name
+    }
+
+    public get price(){
+        return this.props.price
+    }
+
+    public get quantity(){
+        return this.props.quantity
+    }
+}
